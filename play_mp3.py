66ee68fdc8e3a0a0b340c390e89c8bcd9c096f41
cHenYu <Lemon_Tiramisu@Hotmail.com>
2020-05-23 09:29:52 +0800

    Update play_mp3.py

--- play_mp3.py
+++ play_mp3.py
@@ -5,12 +5,18 @@ from pyaudio_play import play_audio
 def trans_mp3_to_wav(file_path):
     print("* translate mp3 to wav")
     song = AudioSegment.from_mp3(file_path)
-    song.export("pingfanzhilu.wav", format("wav"))
+    music = file_path.split('.mp3')[0] + '.wav'
+    song.export(music, format("wav"))
     print("* done translate")
+    return music
+
+def play_mp3(file_path):
+    song = AudioSegment.from_mp3(file_path)
 
 def main():
-    trans_mp3_to_wav("pingfanzhilu.mp3")
-    play_audio("pingfanzhilu.wav")
+    file_path = './audio/平凡之路.mp3'
+    music = trans_mp3_to_wav(file_path)
+    play_audio(music)
 
 if __name__ == '__main__':
     main()
